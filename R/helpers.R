e9a0f092995f5a11c83ebd0ec84b01735d9e8b7c
Atharva Shahane <atharvashahane28@gmail.com>
2025-07-22 17:55:10 -0400

    docs: R-oxygen comments

--- R/helpers.R
+++ R/helpers.R
@@ -3,6 +3,11 @@ greet <- function(name) {
 }
 
 #' Load an RDS model from a public S3 URL directly into memory
+#' 
+#' Downloads and loads an RDS model directly into memory from a public S3 URL
+#' 
+#' @param model_url URL pointing to the RDS file
+#' @return R object from the loaded RDS file
 load_model_from_url <- function(model_url) {
   readRDS(url(model_url))
 }
