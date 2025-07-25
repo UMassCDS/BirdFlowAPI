3b0f68d8afb52fdd402ae77bfb397a7150b8928c
Atharva Shahane <atharvashahane28@gmail.com>
2025-07-25 17:56:01 -0400

    feat: convert relative plumb filepaths to system.file() paths

--- inst/plumber/flow/entrypoint.R
+++ inst/plumber/flow/entrypoint.R
@@ -23,10 +23,10 @@ pr <- pr %>%
       forward()
     }
   }) %>%
-  pr_mount("/hello", plumb("endpoints/hello.R")) %>%
-  pr_mount("/predict", plumb("endpoints/predict.R")) %>%
-  pr_mount("/mock", plumb("endpoints/mock_api.R")) %>%
-  pr_mount("/api", plumb("endpoints/api.R"))
+  pr_mount("/hello", plumb(system.file("plumber", "flow", "endpoints", "hello.R", package = "BirdFlowAPI"))) %>%
+  pr_mount("/predict", plumb(system.file("plumber", "flow", "endpoints", "predict.R", package = "BirdFlowAPI"))) %>%
+  pr_mount("/mock", plumb(system.file("plumber", "flow", "endpoints", "mock_api.R", package = "BirdFlowAPI"))) %>%
+  pr_mount("/api", plumb(system.file("plumber", "flow", "endpoints", "api.R", package = "BirdFlowAPI")))
 
 # Run the API
 pr$run(host = "0.0.0.0", port = 8000)
