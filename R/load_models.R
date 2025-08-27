f9b6272522bf67e6cc22dc5bd46c93385db8b431
Atharva Shahane <atharvashahane28@gmail.com>
2025-08-27 16:53:00 +0530

    feat: update load-models link to sparse data models

--- R/load_models.R
+++ R/load_models.R
@@ -19,7 +19,7 @@ load_models <- function() {
     # load(system.file("data", "species.rda", package = "BirdFlowAPI"))
 
     # Load BirdFlow models
-    BirdFlowR::birdflow_options(collection_url = "https://birdflow-science.s3.amazonaws.com/avian_flu/")
+    BirdFlowR::birdflow_options(collection_url = "https://birdflow-science.s3.amazonaws.com/sparse_avian_flu/")
     index <- BirdFlowR::load_collection_index()
     if(!all(species$species %in% index$species_code)) {
         miss <- setdiff(species$species, index$species_code)
