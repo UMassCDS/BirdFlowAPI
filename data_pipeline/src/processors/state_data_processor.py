b49316f4a9aada6568fc5289a4f4dca86f4a6193
Atharva Shahane <atharvashahane28@gmail.com>
2025-08-28 20:20:44 +0530

    fix: reduce jitter radius

--- data_pipeline/src/processors/state_data_processor.py
+++ data_pipeline/src/processors/state_data_processor.py
@@ -9,7 +9,7 @@ geodata["area_m2"] = geodata["ALAND"] + geodata["AWATER"]
 geodata["centroid"] = geodata.to_crs("+proj=cea").centroid.to_crs(geodata.crs)
 geodata["lat"] = geodata["centroid"].y
 geodata["lon"] = geodata["centroid"].x
-geodata["jitter_radius"] = 0.5 * np.sqrt(geodata["area_m2"] / np.pi)
+geodata["jitter_radius"] = 0.25 * np.sqrt(geodata["area_m2"] / np.pi)
 
 geodata = geodata[["state", "geoid", "lat", "lon", "area_m2", "jitter_radius"]]
 states_csv_save_path = os.path.join("data_pipeline", "data", "original_data", "states.csv")
