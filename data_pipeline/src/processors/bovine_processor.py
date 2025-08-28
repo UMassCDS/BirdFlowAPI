e823d64b605e8f152bc3abb98b89e72dcb54f611
Atharva Shahane <atharvashahane28@gmail.com>
2025-08-28 20:08:12 +0530

    feat: use states.csv in bovine processor

--- data_pipeline/src/processors/bovine_processor.py
+++ data_pipeline/src/processors/bovine_processor.py
@@ -1,22 +1,14 @@
 import pandas as pd
-import json
 import os
 
-state_centroids_path = os.path.join("data_pipeline", "data", "original_data", "state_centroids.json")
-try:
-    with open(state_centroids_path, "r") as file:
-        state_centroids = json.load(file)["data"]
-        state_centroids = pd.DataFrame(state_centroids).rename(columns={"name": "State"})
-except Exception as ex:
-    print(ex)
+states_csv_path = os.path.join("data_pipeline", "data", "original_data", "states.csv")
+states = pd.read_csv(states_csv_path).drop(["index"], axis=1)
+states = states.rename(columns={"state": "State"})
 
 bovine_data_path = os.path.join("data_pipeline", "data", "scraped_data", "bovine.csv")
-try:
-    data = pd.read_csv(bovine_data_path, delimiter="\t", encoding="utf-16")
-except Exception as ex:
-    print(ex)
+data = pd.read_csv(bovine_data_path, delimiter="\t", encoding="utf-16")
 
-data = pd.merge(data, state_centroids, how="left", on="State")
+data = pd.merge(data, states, how="left", on="State")
 data["County Name"] = None
 data["EndDate"] = None
 data["NumInfected"] = None
@@ -25,3 +17,5 @@ data = data[["Confirmed", "State", "County Name", "Production", "EndDate", "NumI
 
 bovine_save_path = os.path.join("data_pipeline", "data", "processed_data", "bovine.json")
 data.to_json(path_or_buf=bovine_save_path, orient="records", indent=4)
+
+print(data.head())
