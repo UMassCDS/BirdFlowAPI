dfb29532546b56fe4aa66c8c4b9066057095b43f
Atharva Shahane <atharvashahane28@gmail.com>
2025-08-28 20:06:59 +0530

    feat: state information CSV file

---
+++ data_pipeline/src/processors/state_data_processor.py
@@ -0,0 +1,16 @@
+import geopandas as gpd
+import numpy as np
+import os
+
+geodata = gpd.read_file(os.path.join("data_pipeline", "data", "original_data", "cb_2024_us_state_500k", "cb_2024_us_state_500k.shp"))
+geodata = geodata.rename(columns={"NAME": "state", "GEOID": "geoid"})
+
+geodata["area_m2"] = geodata["ALAND"] + geodata["AWATER"]
+geodata["centroid"] = geodata.to_crs("+proj=cea").centroid.to_crs(geodata.crs)
+geodata["lat"] = geodata["centroid"].y
+geodata["lon"] = geodata["centroid"].x
+geodata["jitter_radius"] = 0.5 * np.sqrt(geodata["area_m2"] / np.pi)
+
+geodata = geodata[["state", "geoid", "lat", "lon", "area_m2", "jitter_radius"]]
+states_csv_save_path = os.path.join("data_pipeline", "data", "original_data", "states.csv")
+geodata.to_csv(states_csv_save_path, index_label="index")
