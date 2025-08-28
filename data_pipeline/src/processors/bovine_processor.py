868305e69229b0f139ff0722a8b65b0b93f82912
Atharva Shahane <atharvashahane28@gmail.com>
2025-08-28 20:18:12 +0530

    feat: jitter bovine coordinates

--- data_pipeline/src/processors/bovine_processor.py
+++ data_pipeline/src/processors/bovine_processor.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+from ..utils.jitter_function import add_random_jitter
 
 states_csv_path = os.path.join("data_pipeline", "data", "original_data", "states.csv")
 states = pd.read_csv(states_csv_path).drop(["index"], axis=1)
@@ -13,8 +14,21 @@ data["County Name"] = None
 data["EndDate"] = None
 data["NumInfected"] = None
 data["GeoLoc"] = list(zip(data["lat"], data["lon"]))
-data = data[["Confirmed", "State", "County Name", "Production", "EndDate", "NumInfected", "GeoLoc"]]
 
+# Add jitter to each point
+data["GeoLoc"] = data.apply(
+    lambda row: add_random_jitter(row["lat"], row["lon"], row["jitter_radius"]),
+    axis=1
+)
+
+# TEMPORARY CODE - JUST FOR DEMO
+csv_data = data.copy()
+csv_data["jitter_lat"] = csv_data["GeoLoc"].apply(lambda x: x[0])
+csv_data["jitter_lon"] = csv_data["GeoLoc"].apply(lambda x: x[1])
+csv_data = csv_data[["lon", "lat", "jitter_radius", "jitter_lat", "jitter_lon"]]
+csv_data.to_csv("jittered_points.csv")
+
+data = data[["Confirmed", "State", "County Name", "Production", "EndDate", "NumInfected", "GeoLoc"]]
 bovine_save_path = os.path.join("data_pipeline", "data", "processed_data", "bovine.json")
 data.to_json(path_or_buf=bovine_save_path, orient="records", indent=4)
 
