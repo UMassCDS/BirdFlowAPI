70589f14679c5c8bca990458e26413e1c52cd9cb
Ethan Plunkett <ethan@plunkettworks.com>
2025-08-31 16:30:43 -0400

    Add tests for flow

---
+++ tests/testthat/test-flow.R
@@ -0,0 +1,33 @@
+test_that("flow works with single input point", {
+
+  taxa <- "total"
+  week = 15
+  date <- "2022-01-01"
+  lat <- 42
+  lon <- -70
+  direction <- "forward"
+  loc <- paste0(lat, ",", lon)
+
+  expect_no_error(
+    res <- flow(loc = loc, week = week, taxa = taxa,
+                direction = direction, n = 10, save_local = TRUE)
+    )
+
+})
+
+
+
+test_that("flow works with single multi-point input", {
+
+  taxa <- "total"  # all taxa
+  loc <- "42,-70;43,-72;40,-75" # test multi-point
+  week <- 12
+  direction <- "forward"
+
+  expect_no_error(
+    res <- flow(loc = loc, week = week, taxa = taxa,
+                direction = direction, n = 10))
+
+
+
+})
