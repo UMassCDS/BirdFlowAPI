2d0bbc0000ea253dc33bc5c6aee01c3b5ee53c3f
Atharva Shahane <atharvashahane28@gmail.com>
2025-09-09 16:33:02 -0400

    fix: increase WebDriverWait timeout period in bovine scraper

--- data_pipeline/src/scrapers/bovine_scraper.py
+++ data_pipeline/src/scrapers/bovine_scraper.py
@@ -31,7 +31,7 @@ driver = webdriver.Chrome(service=service, options=chrome_options)
 driver.get(url)
 
 # "Download Data" button
-download_btn = WebDriverWait(driver, 15).until(
+download_btn = WebDriverWait(driver, 30).until(
     EC.element_to_be_clickable((By.XPATH, "//div[@role='button' and @title='Download Crosstab']"))
 )
 driver.execute_script("arguments[0].click();", download_btn)
@@ -39,14 +39,14 @@ driver.execute_script("arguments[0].click();", download_btn)
 time.sleep(5)
 
 # "A Table by Confirmation Date" listbox item
-listbox_item = WebDriverWait(driver, 10).until(
+listbox_item = WebDriverWait(driver, 30).until(
     EC.element_to_be_clickable((By.XPATH, "//*[@id='export-crosstab-options-dialog-Dialog-BodyWrapper-Dialog-Body-Id']/div/div[1]/div[2]/div/div/div[7]"))
 )
 driver.execute_script("arguments[0].scrollIntoView(true);", listbox_item)
 driver.execute_script("arguments[0].click();", listbox_item)
 
 # CSV option
-csv_option = WebDriverWait(driver, 10).until(
+csv_option = WebDriverWait(driver, 30).until(
     EC.element_to_be_clickable((By.XPATH, "//label[.//div[text()='CSV']]"))
 )
 driver.execute_script("arguments[0].click();", csv_option)
@@ -54,7 +54,7 @@ driver.execute_script("arguments[0].click();", csv_option)
 time.sleep(2)
 
 # Click "Download" button from pop-up window
-popup_download_btn = WebDriverWait(driver, 10).until(
+popup_download_btn = WebDriverWait(driver, 30).until(
     EC.element_to_be_clickable((By.XPATH, "//*[@id='export-crosstab-options-dialog-Dialog-BodyWrapper-Dialog-Body-Id']/div/div[3]/button"))
 )
 driver.execute_script("arguments[0].click();", popup_download_btn)
