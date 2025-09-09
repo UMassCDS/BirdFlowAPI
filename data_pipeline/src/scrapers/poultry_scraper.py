7ce53ee811da370b2191f42084d855f50707f6eb
Atharva Shahane <atharvashahane28@gmail.com>
2025-09-09 16:32:33 -0400

    fix: increase WebDriverWait timeout period in poultry scraper

--- data_pipeline/src/scrapers/poultry_scraper.py
+++ data_pipeline/src/scrapers/poultry_scraper.py
@@ -31,31 +31,31 @@ driver = webdriver.Chrome(service=service, options=chrome_options)
 driver.get(url)
 
 # "Download Data" button
-download_btn = WebDriverWait(driver, 15).until(
+download_btn = WebDriverWait(driver, 30).until(
     EC.element_to_be_clickable((By.XPATH, "//div[@role='button' and @title='Download Crosstab']"))
 )
 driver.execute_script("arguments[0].click();", download_btn)
 
 # "A Table by Confirmation Date" listbox item
-listbox_item = WebDriverWait(driver, 10).until(
+listbox_item = WebDriverWait(driver, 30).until(
     EC.element_to_be_clickable((By.XPATH, "//div[@role='option' and @title='Affected Totals']"))
 )
 driver.execute_script("arguments[0].scrollIntoView(true);", listbox_item)
 driver.execute_script("arguments[0].click();", listbox_item)
-listbox_item = WebDriverWait(driver, 10).until(
+listbox_item = WebDriverWait(driver, 30).until(
     EC.element_to_be_clickable((By.XPATH, "//div[@role='option' and @title='A Table by Confirmation Date']"))
 )
 driver.execute_script("arguments[0].scrollIntoView(true);", listbox_item)
 driver.execute_script("arguments[0].click();", listbox_item)
 
 # CSV option
-csv_option = WebDriverWait(driver, 10).until(
+csv_option = WebDriverWait(driver, 30).until(
     EC.element_to_be_clickable((By.XPATH, "//label[.//div[text()='CSV']]"))
 )
 driver.execute_script("arguments[0].click();", csv_option)
 
 # Click "Download" button from pop-up window
-popup_download_btn = WebDriverWait(driver, 10).until(
+popup_download_btn = WebDriverWait(driver, 30).until(
     EC.element_to_be_clickable((By.XPATH, '//*[@id="export-crosstab-options-dialog-Dialog-BodyWrapper-Dialog-Body-Id"]/div/div[3]/button'))
 )
 driver.execute_script("arguments[0].click();", popup_download_btn)
