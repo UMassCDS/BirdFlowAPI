d7aa22a6b13153dcb70c610a1ee317580fbe6194
Atharva Shahane <atharvashahane28@gmail.com>
2025-09-11 10:40:01 -0400

    feat: move state variables created by set_s3_config and retrieved by get_s3_config to config environment

--- R/flow.R
+++ R/flow.R
@@ -2,26 +2,27 @@
 #'
 #' Allows user to set S3 credentials and bucket at runtime. If not set, environment variables or IAM role will be used.
 set_s3_config <- function(access_key = NULL, secret_key = NULL, region = NULL, bucket = NULL) {
-  assign(".s3_config", list(
+  config$option <- list(
     access_key = access_key,
     secret_key = secret_key,
     region = region,
     bucket = bucket
-  ), envir = .GlobalEnv)
+  )
 }
 
 #' Get S3 configuration from explicit config, environment variables, or IAM role
 get_s3_config <- function() {
-  config <- if (exists(".s3_config", envir = .GlobalEnv)) get(".s3_config", envir = .GlobalEnv) else list()
-  access_key <- config$access_key %||% Sys.getenv("AWS_ACCESS_KEY_ID", unset = NA)
-  secret_key <- config$secret_key %||% Sys.getenv("AWS_SECRET_ACCESS_KEY", unset = NA)
-  region     <- config$region     %||% Sys.getenv("AWS_DEFAULT_REGION", unset = NA)
-  bucket     <- config$bucket     %||% Sys.getenv("S3_BUCKET_NAME", unset = NA)
+  config_option_list <- config$option %||% list()
+  access_key <- config_option_list$access_key %||% Sys.getenv("AWS_ACCESS_KEY_ID", unset = NA)
+  secret_key <- config_option_list$secret_key %||% Sys.getenv("AWS_SECRET_ACCESS_KEY", unset = NA)
+  region <- config_option_list$region %||% Sys.getenv("AWS_DEFAULT_REGION", unset = NA)
+  bucket <- config_option_list$bucket %||% Sys.getenv("S3_BUCKET_NAME", unset = NA)
   list(access_key = access_key, secret_key = secret_key, region = region, bucket = bucket)
 }
 
 #' Null coalescing operator for config values
 `%||%` <- function(a, b) if (!is.null(a) && !is.na(a) && nzchar(a)) a else b
+
 #' @import BirdFlowR
 
 if(FALSE) {
