56a00481746b168b06efbe91885109e5d5e182f8
Atharva Shahane <atharvashahane28@gmail.com>
2025-09-19 16:16:01 -0400

    feat: move global config vars to s3_config.R

--- R/s3_config.R
+++ R/s3_config.R
@@ -1,5 +1,22 @@
 s3_config <- new.env()
 
+# Define extent of exported data (ai_app_extent)
+s3_config$ai_app_crs <- sf::st_crs("EPSG:3857")
+
+# Note now storing extent as a simple vector (xmin, xmax, ymin, ymax) in EPSG:3857
+s3_config$ai_app_extent <- c(-18924313.4348565, -5565974.53966368,
+                   1118889.97485796, 15538711.0963092)
+
+# s3_configure S3 bucket, its url and the path within it used for flow output
+s3_config$s3_bucket_name <- "avianinfluenza"
+s3_config$s3_flow_path <- "flow/"
+s3_config$s3_flow_url <- "https://avianinfluenza.s3.us-east-2.amazonaws.com/flow/"
+
+# Define local cache for temporary output images, will then be copied to AWS
+s3_config$local_cache <- tempdir()
+if(!file.exists(s3_config$local_cache))
+   dir.create(s3_config$local_cache)
+
 #' Set S3 configuration explicitly
 #'
 #' Allows user to set S3 credentials and bucket at runtime. If not set, environment variables or IAM role will be used.
