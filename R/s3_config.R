50146323938b5d8bf2f5640b5aacaf1be40b6016
Atharva Shahane <atharvashahane28@gmail.com>
2025-09-19 15:36:33 -0400

    chore: remove helper comments and documentation

--- R/s3_config.R
+++ R/s3_config.R
@@ -1,8 +1,5 @@
 s3_config <- new.env()
 
-#' Set S3 configuration explicitly
-#'
-#' Allows user to set S3 credentials and bucket at runtime. If not set, environment variables or IAM role will be used.
 set_s3_config <- function(access_key = NULL, secret_key = NULL, region = NULL, bucket = NULL) {
   assign(".s3_config", list(
     access_key = access_key,
@@ -12,7 +9,6 @@ set_s3_config <- function(access_key = NULL, secret_key = NULL, region = NULL, b
   ), envir = .GlobalEnv)
 }
 
-#' Get S3 configuration from explicit config, environment variables, or IAM role
 get_s3_config <- function() {
   config <- if (exists(".s3_config", envir = .GlobalEnv)) get(".s3_config", envir = .GlobalEnv) else list()
   access_key <- config$access_key %||% Sys.getenv("AWS_ACCESS_KEY_ID", unset = NA)
@@ -22,5 +18,4 @@ get_s3_config <- function() {
   list(access_key = access_key, secret_key = secret_key, region = region, bucket = bucket)
 }
 
-#' Null coalescing operator for config values
 `%||%` <- function(a, b) if (!is.null(a) && !is.na(a) && nzchar(a)) a else b
