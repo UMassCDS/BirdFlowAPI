6aec47a8105888eda5df25365289619d4caa95b6
Atharva Shahane <atharvashahane28@gmail.com>
2025-09-24 16:28:11 -0400

    feat: move all s3_config params to set_s3_config

--- R/s3_config.R
+++ R/s3_config.R
@@ -1,26 +1,34 @@
 s3_config <- new.env()
 
 # Define extent of exported data (ai_app_extent)
-s3_config$ai_app_crs <- sf::st_crs("EPSG:3857")
+# s3_config$ai_app_crs <- sf::st_crs("EPSG:3857")
 
 # Note now storing extent as a simple vector (xmin, xmax, ymin, ymax) in EPSG:3857
-s3_config$ai_app_extent <- c(-18924313.4348565, -5565974.53966368, 1118889.97485796, 15538711.0963092)
+# s3_config$ai_app_extent <- c(-18924313.4348565, -5565974.53966368, 1118889.97485796, 15538711.0963092)
 
 # s3_configure S3 bucket, its url and the path within it used for flow output
-s3_config$s3_bucket_name <- "avianinfluenza"
-s3_config$s3_flow_path <- "flow/"
-s3_config$s3_flow_url <- "https://avianinfluenza.s3.us-east-2.amazonaws.com/flow/"
+# s3_config$s3_bucket_name <- "avianinfluenza"
+# s3_config$s3_flow_path <- "flow/"
+# s3_config$s3_flow_url <- "https://avianinfluenza.s3.us-east-2.amazonaws.com/flow/"
 
 # Define local cache for temporary output images, will then be copied to AWS
-s3_config$local_cache <- tempdir()
-if(!file.exists(s3_config$local_cache))
-   dir.create(s3_config$local_cache)
+# s3_config$local_cache <- tempdir()
+# if(!file.exists(s3_config$local_cache))
+#    dir.create(s3_config$local_cache)
 
 set_s3_config <- function(access_key = NULL, secret_key = NULL, region = NULL, bucket = NULL) {
   s3_config$access_key <- access_key
   s3_config$secret_key <- secret_key
   s3_config$region <- region
   s3_config$bucket <- bucket
+  s3_config$ai_app_crs <- sf::st_crs("EPSG:3857")
+  s3_config$ai_app_extent <- c(-18924313.4348565, -5565974.53966368, 1118889.97485796, 15538711.0963092)
+  s3_config$s3_bucket_name <- "avianinfluenza"
+  s3_config$s3_flow_path <- "flow/"
+  s3_config$s3_flow_url <- "https://avianinfluenza.s3.us-east-2.amazonaws.com/flow/"
+  s3_config$local_cache <- tempdir()
+  if(!file.exists(s3_config$local_cache))
+    dir.create(s3_config$local_cache)
 }
 
 get_s3_config <- function() {
