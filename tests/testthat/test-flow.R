eb1dc89d694f24eb8d5a44c5a71b0fac7e3e60d8
Atharva Shahane <atharvashahane28@gmail.com>
2025-09-28 16:54:43 -0400

    refactor: existing flow tests

--- tests/testthat/test-flow.R
+++ tests/testthat/test-flow.R
@@ -1,5 +1,4 @@
-test_that("flow works with single input point", {
-
+test_that("flow does not throw error with single input point", {
   taxa <- "total"
   week = 15
   date <- "2022-01-01"
@@ -7,27 +6,18 @@ test_that("flow works with single input point", {
   lon <- -70
   direction <- "forward"
   loc <- paste0(lat, ",", lon)
-
   expect_no_error(
     res <- flow(loc = loc, week = week, taxa = taxa,
                 direction = direction, n = 10, save_local = TRUE)
     )
-
 })
 
-
-
-test_that("flow works with single multi-point input", {
-
+test_that("flow does not throw error with single multi-point input", {
   taxa <- "total"  # all taxa
   loc <- "42,-70;43,-72;40,-75" # test multi-point
   week <- 12
   direction <- "forward"
-
   expect_no_error(
     res <- flow(loc = loc, week = week, taxa = taxa,
                 direction = direction, n = 10))
-
-
-
 })
