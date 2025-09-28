5ae20e7d87e8eba30dd4f07e87d1ad6a9e2816d6
Atharva Shahane <atharvashahane28@gmail.com>
2025-09-28 17:00:59 -0400

    feat: test for flow status

--- tests/testthat/test-flow.R
+++ tests/testthat/test-flow.R
@@ -21,3 +21,13 @@ test_that("flow does not throw error with single multi-point input", {
     res <- flow(loc = loc, week = week, taxa = taxa,
                 direction = direction, n = 10))
 })
+
+test_that("status is either success or cached", {
+  taxa <- "ambduc"
+  loc <- "42,-70"
+  week <- 3
+  direction <- "forward"
+  n <- 1
+  res <- flow(loc = loc, week = week, taxa = taxa, n = n, direction = direction)
+  expect_true(res$status == "success" || res$status == "cached")
+})
