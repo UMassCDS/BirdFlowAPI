7eb1983fe605cfbc5f93dc57a2f0631970fad765
Atharva Shahane <atharvashahane28@gmail.com>
2025-09-28 17:35:17 -0400

    refactor: flow tests

--- tests/testthat/test-flow.R
+++ tests/testthat/test-flow.R
@@ -1,63 +1,51 @@
+# Helper function to return a standardized list of paramaters for testing flow
+# These parameters can be used to test fundamental properties of the flow result
+standard_flow_input <- function() {
+  return(list(
+    taxa = "ambduc",
+    loc = "42,-70",
+    week = 15,
+    direction = "forward",
+    n = 10,
+    save_local = TRUE
+  ))
+}
+
 test_that("flow does not throw error with single input point", {
-  taxa <- "total"
-  week = 15
-  date <- "2022-01-01"
-  lat <- 42
-  lon <- -70
-  direction <- "forward"
-  loc <- paste0(lat, ",", lon)
+  params <- standard_flow_input()
   expect_no_error(
-    res <- flow(loc = loc, week = week, taxa = taxa,
-                direction = direction, n = 10, save_local = TRUE)
-    )
+    res <- flow(loc = params$loc, week = params$week, taxa = params$taxa, n = params$n, direction = params$direction, save_local = params$save_local)
+  )
 })
 
 test_that("flow does not throw error with single multi-point input", {
-  taxa <- "total"  # all taxa
-  loc <- "42,-70;43,-72;40,-75" # test multi-point
-  week <- 12
-  direction <- "forward"
+  params <- standard_flow_input()
+  multi_point_loc <- "42,-70;43,-72;40,-75" # test multi-point
   expect_no_error(
-    res <- flow(loc = loc, week = week, taxa = taxa,
-                direction = direction, n = 10))
+    res <- flow(loc = multi_point_loc, week = params$week, taxa = params$taxa, n = params$n, direction = params$direction, save_local = params$save_local)
+  )
 })
 
 test_that("status is either success or cached", {
-  taxa <- "ambduc"
-  loc <- "42,-70"
-  week <- 3
-  direction <- "forward"
-  n <- 1
-  res <- flow(loc = loc, week = week, taxa = taxa, n = n, direction = direction)
+  params <- standard_flow_input()
+  res <- flow(loc = params$loc, week = params$week, taxa = params$taxa, n = params$n, direction = params$direction, save_local = params$save_local)
   expect_true(res$status == "success" || res$status == "cached")
 })
 
 test_that("result taxa matches input", {
-  taxa <- "ambduc"
-  loc <- "42,-70"
-  week <- 3
-  direction <- "forward"
-  n <- 1
-  res <- flow(loc = loc, week = week, taxa = taxa, n = n, direction = direction)
-  expect_true(res$start$taxa == taxa)
+  params <- standard_flow_input()
+  res <- flow(loc = params$loc, week = params$week, taxa = params$taxa, n = params$n, direction = params$direction, save_local = params$save_local)
+  expect_true(res$start$taxa == params$taxa)
 })
 
 test_that("result loc matches input", {
-  taxa <- "ambduc"
-  loc <- "42,-70"
-  week <- 3
-  direction <- "forward"
-  n <- 1
-  res <- flow(loc = loc, week = week, taxa = taxa, n = n, direction = direction)
-  expect_true(res$start$loc == loc)
+  params <- standard_flow_input()
+  res <- flow(loc = params$loc, week = params$week, taxa = params$taxa, n = params$n, direction = params$direction, save_local = params$save_local)
+  expect_true(res$start$loc == params$loc)
 })
 
 test_that("result start week matches input", {
-  taxa <- "ambduc"
-  loc <- "42,-70"
-  week <- 3
-  direction <- "forward"
-  n <- 1
-  res <- flow(loc = loc, week = week, taxa = taxa, n = n, direction = direction)
-  expect_true(res$start$week == week)
+  params <- standard_flow_input()
+  res <- flow(loc = params$loc, week = params$week, taxa = params$taxa, n = params$n, direction = params$direction, save_local = params$save_local)
+  expect_true(res$start$week == params$week)
 })
