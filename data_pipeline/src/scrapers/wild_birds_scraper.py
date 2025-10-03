b89f300a7d0d3301cb6a6379e4d575bccb894c3c
Atharva Shahane <atharvashahane28@gmail.com>
2025-10-03 12:47:17 -0400

    feat: headless option in selenium

--- data_pipeline/src/scrapers/wild_birds_scraper.py
+++ data_pipeline/src/scrapers/wild_birds_scraper.py
@@ -21,6 +21,8 @@ prefs = {
 }
 chrome_options.add_experimental_option("prefs", prefs)
 chrome_options.add_argument("--log-level=3")
+chrome_options.add_argument("--headless=new")
+chrome_options.add_argument("--window-size=1920,1080")
 
 service = Service(CHROMEDRIVER_PATH)
 
