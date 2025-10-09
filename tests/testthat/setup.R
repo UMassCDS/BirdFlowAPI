44cdc79593a8bd5730ecf00b6283555bc4d9afda
Ethan Plunkett <ethan@plunkettworks.com>
2025-10-09 16:10:12 -0400

    fix: use clean local cache for testing

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,2 +1,13 @@
 load_models()
 set_s3_config()
+
+
+# Set the cache directory to a new tempdir for the duration of the tests.
+# and delete it after tests complete.
+# If running individual tests manually don't run this code.
+original_local_temp <- get_s3_config()$local_temp
+local_temp <- withr::local_tempdir("cache", .local_envir = teardown_env())
+s3_config$local_temp <- local_temp
+withr::defer(
+  s3_config$local_temp <- original_local_temp, envir = teardown_env()
+  )
