c6b72899bbff5a9b309d5618c415d21fba0f4027
Atharva Shahane <atharvashahane28@gmail.com>
2025-10-15 14:13:03 -0400

    fix: default value of s3_bucket_name = NULL for local storage

--- R/s3_config.R
+++ R/s3_config.R
@@ -17,7 +17,8 @@ set_s3_config <- function(access_key = NULL, secret_key = NULL, region = NULL, l
 
   s3_config$ai_app_crs <- sf::st_crs("EPSG:3857")
   s3_config$ai_app_extent <- c(-18924313.4348565, -5565974.53966368, 1118889.97485796, 15538711.0963092)
-  s3_config$s3_bucket_name <- "avianinfluenza"
+  # s3_config$s3_bucket_name <- "avianinfluenza"
+  s3_config$s3_bucket_name <- NULL
   s3_config$s3_flow_path <- "flow/"
   s3_config$s3_flow_url <- "https://avianinfluenza.s3.us-east-2.amazonaws.com/flow/"
   s3_config$local_cache <- tempdir()
